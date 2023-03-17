4406703ebf5170f1a5e1adb96defd86bffc742dc
Karthikeyan_A <91019132+KKBUGHUNTER@users.noreply.github.com>
2023-03-17 09:45:09 +0000

    question 1 in assignment 2 updated

--- Assignment_02/Q1_unique.py
+++ Assignment_02/Q1_unique.py
@@ -1,28 +1,37 @@
 import random as ran
 import time as t
 import matplotlib.pyplot as plt
-def unique_ele(arr,n):
-    array=[]
-    i=0
-    while i<n:
-        if(arr[i] not in array):
+
+
+def unique_ele(arr, n):
+    array = []
+    i = 0
+    while i < n:
+        if (arr[i] not in array):
             array.append(arr[i])
-        i+=1
+        i += 1
     return array
+
+
 n = int(input())
-x=[]
-y=[]
-for i in range(1,n+1):
-    a = [ran.randint(0,100) for j in range(i)]
+x = []
+y1 = []
+y2 = []
+for i in range(1, n + 1):
+    a = [ran.randint(0, 100) for j in range(i)]
     num = len(a)
+
     start = t.time()
-    arr = unique_ele(a,num)
+    arr = unique_ele(a, num)
     end = t.time()
     x.append(i)
-    y.append(end-start)
-plt.plot(x,y)
+    y1.append(end - start)
+    
+    start = t.time()
+    arr = set(a)
+    end = t.time()
+    y2.append(end - start)
+    
+plt.plot(x, y1)
+plt.plot(x, y2)
 plt.show()
-
-
-
-
