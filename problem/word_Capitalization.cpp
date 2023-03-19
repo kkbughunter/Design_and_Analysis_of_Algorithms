cec78f93736c8a251bc2845b39c71885b82d55c9
Karthikeyan_A <91019132+KKBUGHUNTER@users.noreply.github.com>
2023-03-19 22:05:55 +0530

    Update word_Capitalization.cpp

--- problem/word_Capitalization.cpp
+++ problem/word_Capitalization.cpp
@@ -1,3 +1,13 @@
+/*Capitalization is writing a word with its first letter as a capital letter. Your task is to capitalize the given word.
+
+Note, that during capitalization all the letters except the first one remains unchanged.
+Input
+
+A single line contains a non-empty word. This word consists of lowercase and uppercase English letters. The length of the word will not exceed 103.
+Output
+
+Output the given word after capitalization. */
+
 #include<iostream>
 using namespace std;
 int main()
