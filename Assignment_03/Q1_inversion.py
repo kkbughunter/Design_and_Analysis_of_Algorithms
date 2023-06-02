5c5977e9103940f9c198e903099f2d60cb0730cb
Karthikeyan_A <91019132+KKBUGHUNTER@users.noreply.github.com>
2023-06-02 17:37:30 +0530

    Delete Assignment_03 directory

--- Assignment_03/Q1_inversion.py
+++
@@ -1,8 +0,0 @@
-def count_inversions1(nums):
-    count = 0
-    for i in range(0, len(nums)):
-        for j in range(i+1,len(nums)):
-            if (nums[i] > nums[j]):
-                print("(",nums[i] , nums[j],") ",end="")
-num=[3,2,8,1]
-count_inversions1(num)
